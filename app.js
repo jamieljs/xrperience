55fd2ea44c45297dc91d9bff95d5c6b0161ac4ca
jamielim <jamie.limjs@gmail.com>
2022-06-20 14:27:22 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 8th Wall, Inc.
+//
+// app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
+// is loaded, and before body.html is loaded.
+
+// hm
+// hello james
+// how much can you see
+// hello james
